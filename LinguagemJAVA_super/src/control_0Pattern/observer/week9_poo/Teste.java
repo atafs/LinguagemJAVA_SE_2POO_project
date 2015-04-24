adf7815f1b26cc61b4d2a03290f08efa6474a391
atafs <atafs@iscte-iul.pt>
2015-04-24 23:57:49 +0100

    m

--- LinguagemJAVA_super/src/control_0Pattern/observer/week9_poo/Teste.java
+++ LinguagemJAVA_super/src/control_0Pattern/observer/week9_poo/Teste.java
@@ -3,7 +3,7 @@ package control_0Pattern.observer.week9_poo;
 public class Teste {
 	public static void main(String[] args) {
 		
-		//TEST
+		//TESTS FOR PATTERN
 		Observador observador = new Observador();
 		Observado observado = new Observado();
 		
