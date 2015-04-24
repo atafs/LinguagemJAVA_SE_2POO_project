b3d5e0d70d67dab76a84e8487ed1aee93773e790
atafs <atafs@iscte-iul.pt>
2015-04-24 23:13:55 +0100

    test commit sourceTree

--- TF POO 2015 Pack V2_3/src/pt/iscte/poo/instalacao/Instalacao.java
+++ TF POO 2015 Pack V2_3/src/pt/iscte/poo/instalacao/Instalacao.java
@@ -10,6 +10,7 @@ import pt.iscte.poo.instalacao.aparelhos.Ligavel;
 import pt.iscte.poo.instalacao.enums.AparelhoEstado;
 import pt.iscte.poo.instalacao.enums.LinhaTomadaEstado;
 
+/** comment */
 public class Instalacao {
 
 	// ATTRIBUTES
