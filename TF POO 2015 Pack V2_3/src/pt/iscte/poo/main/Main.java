d8141d67119b5ae38d35e3246bd88ec6b5d5ddda
atafs <atafs@iscte-iul.pt>
2015-04-27 03:22:09 +0100

    commit test2

--- TF POO 2015 Pack V2_3/src/pt/iscte/poo/main/Main.java
+++ TF POO 2015 Pack V2_3/src/pt/iscte/poo/main/Main.java
@@ -17,7 +17,7 @@ import pt.iscte.poo.instalacao.aparelhos.Ligavel;
 
 public class Main {
 
-	//MAIN ;-)
+	//MAIN test2 ;-)
 	public static void main(String[] args) {
 		JSONParser json = new JSONParser();
 		try {
